5419f5c6351638fd2569c1d3c0262be9084cb2e8
maojiaxing <maojiaxing.s@gmail.com>
2025-03-03 09:37:45 +0000

    sadf

--- profiles/wsl.nix
+++ profiles/wsl.nix
@@ -25,11 +25,17 @@
   environment.sessionVariables = {
     WSLENV = "DISPLAY/u";
     DISPLAY = ":0";
-    LIBGL_ALWAYS_SOFTWARE = "1";  # 修复 OpenGL 加速问题
+    LIBGL_ALWAYS_SOFTWARE = "1";
+    XDG_RUNTIME_DIR = "/run/user/$(id -u)";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /run/user/1000 0700 root root -"
+  ];
 
   services.dbus = {
     enable = true;
+    packages = [ pkgs.dconf ];
     implementation = "broker";  # 使用现代 message broker (默认)
   };
 
@@ -37,8 +43,8 @@
   systemd.user.services.dbus = {
     wantedBy = [ "default.target" ];
     serviceConfig = {
-      Type = "dbus";
-      BusName = "org.freedesktop.DBus";
+      ExecStart = "${pkgs.dbus}/bin/dbus-daemon --session --nofork --nopidfile";
+      Restart = "on-failure";
     };
   };
 }
