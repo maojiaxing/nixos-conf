14fc77c4aab8ced02329777f303e5136f48188cd
maojiaxing <maojiaxing.s@gmail.com>
2025-03-04 12:36:35 +0000

    s试试

--- hosts/boot.nix
+++ hosts/boot.nix
@@ -1,10 +1,14 @@
 { inputs, home-manager, ... }: 
 
-home-manager.nixosModules.home-manager {
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.myuser = import ./home/profiles/default.nix;
-    extraSpecialArgs = { inherit inputs; };
-  };
+{
+
+    
+  home-manager.nixosModules.home-manager {
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      users.myuser = import ./home/profiles/default.nix;
+      extraSpecialArgs = { inherit inputs; };
+    };
+  }；
 }
