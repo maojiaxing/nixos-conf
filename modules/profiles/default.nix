7831f566f9e2e67fc4dcba1faec5a1cdadb1922c
maojiaxing <maojiaxing.s@gmail.com>
2025-08-15 09:11:41 +0000

    aaa

---
+++ modules/profiles/default.nix
@@ -0,0 +1,11 @@
+{ lib, options, config, ...}:
+
+with lib;
+{
+  options.modules.profiles = with types; {
+    hardware = mkOpt (listOf str) [];
+    platform = mkOpt str "linux";
+    role = mkOpt (listOf str) [ "base" ];
+    user = mkOpt attr {};
+  };
+}
