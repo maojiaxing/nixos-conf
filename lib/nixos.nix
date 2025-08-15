09f6f8fe7a8dc32c2b3ca81bc68524fad5abc93e
maojiaxing <maojiaxing.s@gmail.com>
2025-08-15 07:50:58 +0000

    aaa

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -1,4 +1,4 @@
-{ lib, attrs, modules, ... }:
+{ self, lib, attrs, modules, ... }:
 
 with builtins;
 with lib;
@@ -14,121 +14,133 @@ rec {
     type = "app";
   };
 
-  # mapHosts :: dir -> attrs
-  #
-  # 遍历指定目录下的主机模块，导入配置。
-  mapHosts = dir:
-    mapModules dir (path: {
-      inherit path;
-      config = import path;
-    });
-
   # mkPkgs :: system -> nixpkgs -> overlays -> pkgs
   #
   # 根据系统、nixpkgs 和 overlays 构建 pkgs。
-  mkPkgs = system: nixpkgs: overlays: import nixpkgs {
+  mkPkgs = { system, pkgsPath, overlays ? [] }:
     inherit system overlays;
-
     config.allUnfree = true;
   };
 
-  # mkHostConfig :: inputs -> overlays -> hostname -> {path, config} -> nixosSystem
-  #
-  # 构建单个主机的 nixosSystem 配置。
-  mkHostConfig = inputs: overlays: hostname: {path, config}:
-    let
-      system = config.system or "x86_64-linux";
-      pkgs = mkPkgs system inputs.nixpkgs overlays;
 
-      selfArgs = inputs.self // {
-        dir = toString input.self;
-        rev = inputs.self.rev or "dirty";
-        inherit (inputs) inputs;
-        packages = inputs.self.packages.${system} or {};
-        apps = inputs.self.apps.${system} or {};
-        devShells = inputs.self.devShells.${system} or {};
+  mkHost = { hostname, hostDef, inputs, overlays}:
+    let
+      path = hostDef.path;
+
+      allNixosModules = filterMapAttrs
+        (_: i: i ? nixosModules)
+        (_: i: i.nixosModules)
+        inputs;
+
+      hostConfig = hostDef.config {
+        inherit lib;
+        nixosModules = allNixosModules;
+        self = inputs.self;
       };
 
-      host = config { inherit lib; self = selfArgs;};
-      storage = inputs.flake.storage.${hostName} or {};
+      system = hostConfig.system;
+
+      pkgs = buildPkgs {
+        inherit system overlays;
+        pkgsPath = inputs.nixpkgs;
+      } // {
+        unstable = buildPkgs {
+          inherit system overlays;
+          pkgsPath = inputs.nixpkgs-unstable;
+        };
+      };
+
+      self' = self // {
+        modules = allNixosModules;
+        packages = self.packages.${system};
+        devShell = self.devShell.${system};
+        apps = self.apps.${system};
+      };
     in
       lib.nixosSystem {
         inherit system;
-
         specialArgs = {
-          self = selfArgs;
+          self = self';
         };
 
-        modules = [
-          inputs.disko.nixosModules.disko
+        modules =
+          [
+            inputs.disko.nixosModules.disko
+            (if isFunction inputs.storage
+             then (attrs: { disko.devices = inputs.storage attrs; })
+             else { disko.devices = inputs.storage; })
+            {
+              nixpkgs.pkgs = pkgs;
+              # 主机名直接使用定义的名称
+              networking.hostName = mkDefault hostName;
+            }
+            ../.
+          ]
+          ++ (hostConfig.imports or [])
+          ++ [{ modules = hostConfig.modules or {}; }]
+          ++ [ (hostConfig.config or {}) (hostConfig.hardware or {}) ];
+      }
 
-          (if isFunction storage
-           then (attrs: { disko.devices = storage attrs; })
-           else { disko.devices = storage; })
-          {
-            nixpkgs.pkgs = pkgs;
-            networking.hostName = mkDefault hostName;
-          }
+   buildPerSystemOutputs = { systems, overlays, inputs, flake, self }:
+    let
+      outputsForSystem = system:
+        let
+          pkgs = buildPkgs {
+            inherit system overlays;
+            pkgsPath = inputs.nixpkgs;
+          };
+          packageBuilder = packageAttrs:
+            mapFilterAttrs
+              (_: v: pkgs.callPackage v { self = self.packages.${system} or {}; })
+              (_: v: !(v ? meta.platforms) || (elem system v.meta.platforms))
+              packageAttrs;
+        in
+          filterAttrs (_: v: v != {}) {
+            apps = flake.apps or {};
+            checks = packageBuilder (flake.checks or {});
+            devShells = packageBuilder (flake.devShells or {});
+            packages = packageBuilder (flake.packages or {});
+          };
+    in
+      mergeAttrs' (map (system: mapAttrs (name: value: { ${name}.${system} = value; }) (outputsForSystem system)) systems);
 
-          ../default.nix
-        ]
-        ++ (host.imports or [])
-        ++ [ { modules = host.modules or {}; } ]
-        ++ [ (host.config or {}) (host.hardware or {}) ];
+  mkFlake: input @ {
+    self,
+    nixpkgs ? self.inputs.nixpkgs,
+    nixpkgs-unstable ? self.inputs.nixpkgs-unstable or nixpkgs,
+    disko ? self.inputs.disko,
+    ...
+  }: flake @ {
+    hosts ? {},
+    overlays ? {},
+    systems ? [ "x86_64-linux" ]
+  }:
+    let
+      overlayValues = attrValues (flake.overlays or {});
+
+      nixosConfigurations = mapAttrs
+        (hostName: hostDef: buildHost {
+          inherit hostName hostDef;
+          inherit inputs;
+          overlays = overlayValues;
+        })
+        hosts;
+
+      perSystemOutputs = buildPerSystemOutputs {
+        inherit systems self;
+        flake = flake;
+        inputs = inputs;
+        overlays = overlayValues;
       };
 
-  # mkSystemPackages :: system -> nixpkgs -> overlays -> packageAttrs -> pkgs
-  #
-  # 构建系统包集合，按平台过滤。
-  mkSystemPackages = system: nixpkgs: overlays: packageAttrs:
-    let
-      pkgs = mkPkgs system nixpkgs overlays;
-      withPkgs = mapFilterAttrs
-        (_: v: pkgs.callPackage v {
-          self = self.packages.${system} or {};
-        })
-        (_: v: !(v ? meta.platforms) || (elem system v.meta.platforms))
-        packageAttrs;
-    in
-      withPkgs;
-
-  # mkSystemOutput :: inputs -> flake -> overlays -> systems -> attrs
-  #
-  # 构建跨系统输出，包含 apps、checks、devShells、packages。
-  mkSystemOutputs = inputs: flake: overlays: systems:
-    map (system:
-      filterAttrs (_: v: v.${system} != {}) {
-        apps.${system} = flake.apps or {};
-        checks.${system} = mkSystemPackages system inputs.nixpkgs overlays (flake.checks or {});
-        devShells.${system} = mkSystemPackages system inputs.nixpkgs overlays (flake.devShells or {});
-        packages.${system} = mkSystemPackages system inputs.nixpkgs overlays (flake.packages or {});
-      }
-    ) systems;
-
-  # mkFlake :: inputs -> flake -> attrs
-  #
-  # 构建 flake 输出，包含主机配置和跨系统输出。
-  mkFlake = inputs: flake:
-    let
-      overlays = attrValues (flake.overlays or {});
-      systems = flake.systems or [ "x86_64-linux" ];
-
-      # 构建host配置
-      nixosConfigurations = mapAttrs
-        (mkHostConfig inputs overlays)
-        (flake.hosts or {});
-
-      # 构建跨系统输出
-      systemOutputs = mkSystemOutputs inputs flake overlays systems;
-
-       # 过滤并保留其他 flake 属性
       passthroughAttrs = filterAttrs (n: _: !elem n [
         "apps" "bundlers" "checks" "devices" "devShells" "hosts" "modules"
         "packages" "storage" "systems"
       ]) flake;
 
     in
-      passthroughAttrs
-      // { inherit nixosConfigurations; nixosModules = flake.modules or {}; }
-      // (mergeAttrs' systemOutputs);
+      passthroughAttrs // {
+        inherit nixosConfigurations;
+        nixosModules = flake.modules or {};
+      } // perSystemOutputs;
 }
