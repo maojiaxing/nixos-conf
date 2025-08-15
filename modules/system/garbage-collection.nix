01efa7b97edb290f002d3f83b43e5abffded383c
maojiaxing <maojiaxing.s@gmail.com>
2025-08-15 06:45:47 +0000

    fix: 移除 garbage-collection.nix 中的冗余代码，简化配置结构

--- modules/system/garbage-collection.nix
+++ modules/system/garbage-collection.nix
@@ -1,36 +1,5 @@
-{ lib, config, options, ... }: 
+{ lib, config, options, ... }:
 
-with lib;
-let cfg = config.modules.system.gc;
-in {
-  options.modules.system.gc = {
-    enable = mkBoolOpt true;
-  };
-
-  mkIf (config.modules.system.gc.enable == true) {
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
-  systemd = {
-    services.clear-log = {
-      description = "Clear >1 month-old logs every week";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=21d";
-      };
-    };
-
-    timers.clear-log = {
-      wantedBy = [ "timers.target" ];
-      partOf = [ "clear-log.service" ];
-      timerConfig.OnCalendar = "weekly UTC";
-    };
-  };
+{
+  
 }
-}
-
-
-
