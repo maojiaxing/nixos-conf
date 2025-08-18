32e79685b66ac5b200faf26a4af4f0b6122995b8
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 06:09:26 +0000

    refactor

--- default.nix
+++ default.nix
@@ -1,40 +1,36 @@
 { self, lib, config, options, pkgs, ...}:
 
 with lib;
-let
-  mainModule = {lib, config, options, pkgs, ...}:
-    {
-      modules = mapModulesRec' ./modules import;
-      imports = builtins.trace "aaaaaaaaaaaaaaaa ${builtins.toString modules}" modules;
-
-      options = with types; {
-        modules = {};
-
-        user = mkOpt attrs { name = ""; };
-      };
-
-      config = {
-        assertions = [{
-          assertion = config.user ? name;
-          message = "config.user.name is not set!";
-        }];
-
-        user = {
-          description = mkDefault "The primary user account";
-          extraGroups = [ "wheel" ];
-          isNormalUser = true;
-          home = "/home/${config.user.name}";
-          group = "users";
-          uid = 1000;
-        };
-        users.users.${config.user.name} = mkAliasDefinitions options.user;
-      };
-    };
-in
 {
-  imports = [ mainModule ];
+  imports = mapModulesRec' ./modules import;
 
-  _module.args = {
-    lib = lib // self.lib;
+  options = with types; {
+    modules = {};
+
+    user = mkOption {
+      description = "User-specific attributes, such as name and home directory.";
+      type = attrs;
+      default = { name = ""; };
+    }
   };
-}
+
+  config = {
+    assertions = [
+      {
+        assertion = config.user ? name;
+        message = "config.user.name is not set!";
+      }
+    ];
+
+    user = {
+      description = mkDefault "The primary user account";
+      extraGroups = [ "wheel" ];
+      isNormalUser = true;
+      home = "/home/${config.user.name}";
+      group = "users";
+      uid = 1000;
+    };
+
+    users.users.${config.user.name} = mkAliasDefinitions options.user;
+  };
+};
--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -1,12 +1,34 @@
 { lib, options, config, ...}:
 
-with lib;
-with types;
 {
   options.modules.profiles = {
-    hardware = mkOpt (listOf str) [];
-    platform = mkOpt (enum [ "linux" "darwin" "wsl" ]) "linux";
-    role = mkOpt (listOf str) [ "base" ];
-    user = mkOpt attrs { name = "maojiaxing"; };
+
+    hardware = lib.mkOption {
+      description = "A list of hardware definition for the custom host.";
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      example = lib.literalExpression '' [ intel ] '';
+    };
+
+    platform = lib.mkOption {
+      description = "The platform or operating system type for the host.";
+      type = lib.types.enum [ "linux" "darwin" "wsl" ];
+      default = "linux";
+      example = "darwin";
+    };
+
+    role = lib.mkOption {
+      description = "A list of roles to apply to the host.";
+      type = lib.types.listOf lib.types.str;
+      default = [ "base" ];
+      example = lib.literalExpression ''[ "base" ]'';
+    };
+
+    user = lib.mkOption {
+      description = "User-specific attributes, such as name and home directory.";
+      type = lib.types.attrs;
+      default = { name = "maojiaxing"; };
+      example = lib.literalExpression ''{ name = "nix-user"; home = "/home/nix-user"; }'';
+    };
   };
 }
