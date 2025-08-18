c7c085815286d4693b0e55af3a4995f922935e5a
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 09:26:51 +0000

    aaa

--- modules/profiles/platform/wsl.nix
+++ modules/profiles/platform/wsl.nix
@@ -3,9 +3,6 @@
 with lib;
 mkMerge [
   (mkIf (config.modules.profiles.platform == "wsl") {
-    imports = [
-      inputs.nixos-wsl.nixosModules.default
-    ];
 
     wsl.enable = true;
     wsl.defaultUser = config.user.name;
