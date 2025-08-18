a4c141cb208bc1f2d56df485740d31a914bc05ff
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 06:36:40 +0000

    sdf

--- modules/profiles/hardware/cpu/_amd.nix
+++ modules/profiles/hardware/cpu/_amd.nix
@@ -1,16 +1,17 @@
 { lib, config, pkg, ... }:
 
-with lib;
-let hardware = config.modules.profiles.hardware;
-in mkMerge [
-  (mkIf (any (s: hasPrefix "cpu/amd" s) hardware) {
-    hardware.cpu.amd.updateMicrocode =
-      mkDefault config.hardware.enableRedistributableFirmware;
+# with lib;
+# let hardware = config.modules.profiles.hardware;
+# in mkMerge [
+#   (mkIf (any (s: hasPrefix "cpu/amd" s) hardware) {
+#     hardware.cpu.amd.updateMicrocode =
+#       mkDefault config.hardware.enableRedistributableFirmware;
 
-      boot.kernelParams = [ "amd_pstate=active" ];
-  })
+#       boot.kernelParams = [ "amd_pstate=active" ];
+#   })
 
-  (mkIf (elem "cpu/amd/raphael" hardware) {
-    boot.kernelParams = [ "amdgpu.sg_display=0" ];
-  })
-]
+#   (mkIf (elem "cpu/amd/raphael" hardware) {
+#     boot.kernelParams = [ "amdgpu.sg_display=0" ];
+#   })
+# ]
+{}
--- modules/profiles/hardware/cpu/_intel.nix
+++ modules/profiles/hardware/cpu/_intel.nix
@@ -1,18 +1,19 @@
 { lib, config, pkgs, ...}:
 
-with lib;
-let hardware = config.modules.profiles.hardware;
-in mkMerge [
-  (mkIf (any (s: hasPrefix "cpu/intel" s) hardware) {
-    hardware.cpu.intel.updateMicrocode =
-      mkDefault config.hardware.enableRedistributableFirmware;
-  })
+# with lib;
+# let hardware = config.modules.profiles.hardware;
+# in mkMerge [
+#   (mkIf (any (s: hasPrefix "cpu/intel" s) hardware) {
+#     hardware.cpu.intel.updateMicrocode =
+#       mkDefault config.hardware.enableRedistributableFirmware;
+#   })
 
-  (mkIf (elem "cpu/intel/sandy-bridge" hardware) {
-    boot.kernelParams = [ "i915.enable_rc6=7" ];
-  })
+#   (mkIf (elem "cpu/intel/sandy-bridge" hardware) {
+#     boot.kernelParams = [ "i915.enable_rc6=7" ];
+#   })
 
-  (mkIf (elem "cpu/intel/kaby-lake" hardware) {
-    boot.kernelParams = [ "i915.enable_fbc=1" "i915.enable_psr=2" ];
-  })
-]
+#   (mkIf (elem "cpu/intel/kaby-lake" hardware) {
+#     boot.kernelParams = [ "i915.enable_fbc=1" "i915.enable_psr=2" ];
+#   })
+# ]
+{}
