51552bac4f40056e3068ac3ce1ed7a27ff8b0669
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 08:39:22 +0000

    asdf

--- modules/profiles/platform/wsl.nix
+++ modules/profiles/platform/wsl.nix
@@ -2,11 +2,13 @@
 
 with lib;
 mkIf (config.modules.profiles.platform == "wsl") {
-  imports = [
-      inputs.nixos-wsl.nixosModules.default
-  ];
+
 
   config = {
+    imports = [
+      inputs.nixos-wsl.nixosModules.default
+    ];
+
     wsl.enable = true;
     wsl.defaultUser = config.user.name;
   };
