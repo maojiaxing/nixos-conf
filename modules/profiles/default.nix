e3cd5c72a9715e085171de4d19ddf319b6f46871
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 01:19:53 +0000

    sdfds

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -1,11 +1,11 @@
 { lib, options, config, ...}:
 
 with lib;
-{
+mkMerge [{
   options.modules.profiles = with types; {
     hardware = mkOpt (listOf str) [];
-    platform = mkOpt enum [ "linux" "darwin" "wsl" ] "linux";
+    platform = mkOpt (enum [ "linux" "darwin" "wsl" ]) "linux";
     role = mkOpt (listOf str) [ "base" ];
     user = mkOpt attrs {};
   };
-}
+}]
