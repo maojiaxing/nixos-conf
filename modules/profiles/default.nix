4c95053ab9ae726a797648ed4952acb46dd02e03
maojiaxing <maojiaxing.s@gmail.com>
2025-08-18 02:03:54 +0000

    sadf

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -2,12 +2,12 @@
 
 with lib;
 with types;
-mkMerge [
-  {
-    options.modules.profiles = {
-      hardware = mkOpt (listOf str) [];
-      platform = mkOpt (enum [ "linux" "darwin" "wsl" ]) "linux";
-      role = mkOpt (listOf str) [ "base" ];
-      user = mkOpt attrs {};
-  }
-];
+{
+  options.modules.profiles = {
+    hardware = mkOpt (listOf str) [];
+    platform = mkOpt (enum [ "linux" "darwin" "wsl" ]) "linux";
+    role = mkOpt (listOf str) [ "base" ];
+    user = mkOpt attrs {};
+  };
+}
+
