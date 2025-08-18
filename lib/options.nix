25f15da2cad977b7a71a8239d46593b63f7a586b
maojiaxing <maojiaxing@mail.com>
2025-08-18 01:19:05 +0000

    asdfds

--- lib/options.nix
+++ lib/options.nix
@@ -13,14 +13,11 @@ rec {
   mkOpt' = type: default: description:
   let
     isEnum = lib.isAttr type && type ? "_type" && type._type == "enum";
-    in {
-      assert lib.assertMsg (!isEnum || lib.elem default type.values)
-      "The default value '${toString default}' is not in the list of allowed enum values.";
+    in
       mkOption {
         type = if isEnum then lib.types.enum type.values else type;
         inherit default description;
       };
-    };
 
   mkBoolOpt = default: mkOption {
     inherit default;
