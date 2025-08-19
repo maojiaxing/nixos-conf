d384fb7892e5bb7091fa815e4da7a7b02b74360c
maojiaxing <maojiaxing.s@gmail.com>
2025-08-19 03:24:25 +0000

    aaa

--- modules/profiles/platform/linux.nix
+++ modules/profiles/platform/linux.nix
@@ -2,29 +2,27 @@
 
 with lib;
 mkIf (config.modules.profiles.platform == "linux") {
-  config = {
-    catppuccin.mako.enable = false;
+  catppuccin.mako.enable = false;
 
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  systemd = {
+    services.clear-log = {
+      description = "Clear >1 month-old logs every week";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=21d";
+      };
     };
 
-    systemd = {
-      services.clear-log = {
-        description = "Clear >1 month-old logs every week";
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.systemd}/bin/journalctl --vacuum-time=21d";
-        };
-      };
-
-      timers.clear-log = {
-        wantedBy = [ "timers.target" ];
-        partOf = [ "clear-log.service" ];
-        timerConfig.OnCalendar = "weekly UTC";
-      };
+    timers.clear-log = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "clear-log.service" ];
+      timerConfig.OnCalendar = "weekly UTC";
     };
   };
 }
