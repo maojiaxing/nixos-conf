06058edd9dd7ed7f56d53293ba9401bf18832243
maojiaxing <maojiaxing.s@gmail.com>
2025-08-19 07:19:26 +0000

    aa

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       args = {
         inherit self;
         inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {};
+        pkgs = import nixpkgs {system = "x86_64-linux"; };
       };
 
       lib = import ./lib args;
