0191c84f86491f610796a16d5bf817e5afabe193
maojiaxing <maojiaxing.s@gmail.com>
2025-08-27 00:12:03 +0800

    Update cpu.nix

--- modules/profiles/hardware/cpu.nix
+++ modules/profiles/hardware/cpu.nix
@@ -3,16 +3,18 @@
 with lib;
 let 
   cpu = config.modules.profiles.hardware.cpu;
+  
 in {
   options.modules.profiles.hardwrae = {
     cpu = mkOpt' (types.enum [ "intel" "amd" "arm" "none" ]) "none" "The vendor/architecture of the CPU for this host.";
   };
 
   (lib.mkIf (cpu == "intel") {
-    hardware.cpu.intel.updateMicrocode = true;
+    hardware.cpu.intel.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
   });
 
   (lib.mkIf (cpu == "amd") {
-    hardware.cpu.amd.updateMicrocode = true;
+    hardware.cpu.amd.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
+    boot.kernelParams = [ "amd_pstate=active" ];
   });
 }
