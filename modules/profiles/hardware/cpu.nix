992406371a35c300766a194764f834a564bcfc5c
maojiaxing <maojiaxing.s@gmail.com>
2025-08-27 09:38:30 +0800

    aaa

--- modules/profiles/hardware/cpu.nix
+++ modules/profiles/hardware/cpu.nix
@@ -8,11 +8,11 @@ in {
     cpu = mkOpt' (types.enum [ "intel" "amd" "arm" "none" ]) "none" "The vendor/architecture of the CPU for this host.";
   };
 
-  (lib.mkIf (cpu == "intel") {
+  (mkIf cpu == "intel" {
     hardware.cpu.intel.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
   });
 
-  (lib.mkIf (cpu == "amd") {
+  (mkIf cpu == "amd" {
     hardware.cpu.amd.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
     boot.kernelParams = [ "amd_pstate=active" ];
   });
