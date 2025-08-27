966c9ee41ee00db0aba387c5c8cb7033d41ecbc7
maojiaxing <maojiaxing.s@gmail.com>
2025-08-27 10:36:15 +0800

    aaa

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -3,7 +3,7 @@
 with lib;
 {
   options.modules.profiles = {
-    hardware = mkOpt' types.attrs {} "A set of hardware definition for the custom host.";
+    #hardware = mkOpt' types.attrs {} "A set of hardware definition for the custom host.";
     platform = mkOpt' (types.enum [ "linux" "darwin" "wsl" ]) "linux" "The platform or operating system type for the host.";
     roles = mkOpt' (types.listOf types.str) [ "base" ] "A list of roles to apply to the host.";
   };    
