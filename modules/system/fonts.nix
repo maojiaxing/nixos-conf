763eea2d5b69d0dc7d565c84948311972f3575a1
maojiaxing <maojiaxing.s@gmail.com>
2025-08-28 17:20:07 +0800

    aa

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -7,12 +7,9 @@
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
     noto-fonts-emoji
-    nerd-fonts.symbols-only
-    nerd-fonts.hurmit
-    nerd-fonts.monofur
     lxgw-wenkai
     maple-mono-variable
   ];
-  
+
   fonts.fontDir.enable = true;
 }
